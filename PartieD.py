9eb6015c672f64dd87f57693ccaeb7ffb8eb9104
watson67 <thibaut.waechter@gmail.com>
2022-05-28 00:29:20 +0200

    aa

---
+++ PartieD.py
@@ -0,0 +1,30 @@
+
+from turtle import color
+from matplotlib import colors
+from mpl_toolkits import mplot3d
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+import random
+
+#------------------------------------------------------------------------------
+#         D.   Calcul de l'inverse d'une matrice
+#------------------------------------------------------------------------------
+
+#-------------------------------- Question 1  ---------------------------------
+
+
+
+#-------------------------------- Question 2  ---------------------------------
+
+def G(y,A,b):
+    return 2*(A*y-b)
+
+def grad_pas_opti(y0, A, b):
+    pass
+
+A = [1,3]
+B=[5,4]
+print(A.dot(B))
+#-------------------------------- Question 3  ---------------------------------
+
