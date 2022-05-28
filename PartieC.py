71a1baeb2b7e1b2d73a7a9a16d616d4b495defce
PalisseVolia <palissev@gmail.com>
2022-05-28 18:08:49 +0200

    Update PartieC.py

--- PartieC.py
+++ PartieC.py
@@ -3,6 +3,7 @@
 from turtle import color
 from matplotlib import colors
 from matplotlib.pyplot import title
+from mpl_toolkits.mplot3d import axes3d
 from mpl_toolkits import mplot3d
 import numpy as np
 import matplotlib.pyplot as plt
@@ -255,13 +256,64 @@ print("Pour h(x,y) avec x0 = 0 et y0 = 0 :")
 p = gradpc(0.0001,100,-0.1,0,0,dh_dx,dh_dy,"h(x,y)")
 print(p)
 print("h(x,y) = ")
-print( h(p[0],p[1]) )
+print(h(p[0],p[1]))
 
 print("Pour g227(x,y) avec x0 = 7 et y0 = 1.5 :")
 p = gradpc(0.0001,100,-0.1,7,1.5,dg_227_dx,dg_227_dy,"g_2,2/7")
 print(p)
 print(g_227(p[0],p[1]))
-#Le minimum global de g_22/7 est obtenu pour le couple (0,0)
+
+# tracé des courbes de niveau en 2D et 3D
+def plot_XD_LN_gradpc(eps, m, u, x0, y0, df1, df2, s, func):
+    XN = []
+    YN = []
+    X = []
+    Y = []
+    Z = []
+    nb_iteration = 0
+    grad = np.zeros(2)
+    grad[0] = df1(x0,y0) 
+    grad[1] = df2(x0,y0)
+    point = [x0 , y0]
+    while (norme_gradient(grad)>eps) and (nb_iteration < m) :
+        XN.append(point[0])
+        YN.append(point[1])
+        point = point + u * grad
+        grad[0] = df1(point[0],point[1])
+        grad[1] = df2(point[0],point[1])
+        nb_iteration += 1
+    # définition des points de la grille
+    f = np.vectorize(func)
+    X = np.arange(-10, 10, 0.01)
+    Y = np.arange(-10, 10, 0.01)
+    Z = np.arange(len(XN)*len(YN)).reshape(len(XN),len(YN))
+    X, Y = np.meshgrid(XN, YN)
+    Z = f(X,Y)
+    #tracé des courbes de niveau 2D
+    fig10 = plt.figure(figsize = (10,2))
+    plt.axis('equal')
+    plt.contour(X, Y, Z, 15)
+    plt.title(s + " 2D", fontsize = 13)
+    plt.xlabel('Xn', fontsize = 11)
+    plt.ylabel('Yn', fontsize = 11)
+    plt.show()
+    # tracé de courbes de niveau 3D
+    fig20 = plt.figure(figsize = (10,10))
+    ax = plt.axes(projection='3d')
+    ax.plot_surface(X, Y, Z, rstride=1, cstride=1,
+    cmap='coolwarm', edgecolor='none', alpha=0.6)
+    ax.set_title(s + " 3D", fontsize = 13)
+    ax.contour(X, Y, Z, 20, colors="k", linestyles="solid")
+    ax.set_xlabel('Xn', fontsize = 11)
+    ax.set_ylabel('Yn', fontsize = 11)
+    ax.set_zlabel('Z', fontsize = 10)
+    ax.view_init(30, 120)
+
+# Pour la fonction h tous les X du gradient sont égaux à 0, donc on ne peut pas tracer la fonction h sur une grille :
+# plot_XD_LN_gradpc(0.0001,100,-0.1,0,0,dh_dx,dh_dy,"Contour gradient h(x,y)",h)
+# En revanche on peux tracer la convergence de la fonction g227 sur une grille :
+plot_XD_LN_gradpc(0.0001,100,-0.1,7,1.5,dg_227_dx,dg_227_dy,"Contour gradient g_2,2/7",g_227)
+# Le minimum global de g_22/7 est obtenu pour le couple (0,0)
 
 #-------------------------------- Question 7  ---------------------------------
 
