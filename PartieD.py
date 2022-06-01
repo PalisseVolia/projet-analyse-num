0df8fae6b6ed3d37b8c7bf696dfd778a6df5d3fc
Volia Palisse <81376099+PalisseVolia@users.noreply.github.com>
2022-06-01 10:38:44 +0200

    indentation et var

--- PartieD.py
+++ PartieD.py
@@ -26,30 +26,30 @@ b = np.array([-3,5])
 A = np.array([[9,0],[0,9]])
 
 def G(y):
-  return 2*(np.matmul(A,y)-b)
+	return 2*(np.matmul(A,y)-b)
 
 def rho(y):
-  if np.array_equal(G(y),[0,0]):
-    return 0
-  else:
-     num = (np.linalg.norm(G(y)))**2
-     den = 2*np.matmul(np.transpose(G(y)),np.matmul(A,G(y)))
-     return num/den
+	if np.array_equal(G(y),[0,0]):
+		return 0
+	else:
+		num = (np.linalg.norm(G(y)))**2
+		den = 2*np.matmul(np.transpose(G(y)),np.matmul(A,G(y)))
+		return num/den
 
-dernierY = np.array([2,2])
+Yn = np.array([2,2])
 
-Y = dernierY - rho(dernierY)*G(dernierY)
-dist = np.linalg.norm(Y-dernierY)
+Y = Yn - rho(Yn)*G(Yn)
+dist = np.linalg.norm(Y-Yn)
 while (nb_itérations <= m and dist > eps):
-  nb_itérations += 1
-  dernierY = Y
-  Y = dernierY - rho(dernierY)*G(dernierY)
-  dist = np.linalg.norm(Y-dernierY)
-  print(dist)
+	nb_itérations += 1
+	Yn = Y
+	Y = Yn - rho(Yn)*G(Yn)
+	dist = np.linalg.norm(Y-Yn)
+	print(dist)
   
 plt.plot(x,err_balayage1,color="r")
 print("nb_itérations : ", nb_itérations)
-print("Y : ",dernierY)
+print("Y : ",Yn)
 
 #-------------------------------- Question 3  ---------------------------------
 
