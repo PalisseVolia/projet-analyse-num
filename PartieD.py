60031e440a90accd074a9af484e44d614ca57412
watson67 <thibaut.waechter@gmail.com>
2022-06-01 10:22:52 +0200

    ggg

--- PartieD.py
+++ PartieD.py
@@ -17,14 +17,41 @@ import random
 
 #-------------------------------- Question 2  ---------------------------------
 
-def G(y,A,b):
-    return 2*(A*y-b)
+import math
+import numpy as np
 
-def grad_pas_opti(y0, A, b):
-    pass
 
-A = [1,3]
-B=[5,4]
-print(A.dot(B))
+eps = math.pow(10,-6)
+m = 100
+compteur = 0
+
+b = np.array([-3,5])
+A = np.array([[9,0],[0,9]])
+
+def G(y):
+  return 2*(np.matmul(A,y)-b)
+
+def rho(y):
+  if np.array_equal(G(y),[0,0]):
+    return 0
+  else:
+     num = (np.linalg.norm(G(y)))**2
+     den = 2*np.matmul(np.transpose(G(y)),np.matmul(A,G(y)))
+     return num/den
+
+dernierY = np.array([2,2])
+
+Y = dernierY - rho(dernierY)*G(dernierY)
+dist = np.linalg.norm(Y-dernierY)
+while (compteur <= m and dist > eps):
+  compteur += 1
+  dernierY = Y
+  Y = dernierY - rho(dernierY)*G(dernierY)
+  dist = np.linalg.norm(Y-dernierY)
+  print(dist)
+  
+plt.plot(x,err_balayage1,color="r")
+print("compteur : ", compteur)
+print("Y : ",dernierY)
 #-------------------------------- Question 3  ---------------------------------
 
